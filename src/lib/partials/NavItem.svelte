c271a30828e9568862af8ced50bf340323f5d476
Ammar Iqbal <ammariqbal043@gmail.com>
2024-01-05 12:45:23 +0500

    fix: navitem collapses on child click

--- src/lib/partials/NavItem.svelte
+++ src/lib/partials/NavItem.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { navigate } from 'astro:transitions/client'
   import { slide } from 'svelte/transition'
 
   type $$Props = {
@@ -21,23 +22,25 @@
     <slot />
   </a>
 {:else}
-  <button on:click={() => (isOpen = !isOpen)}>
-    <div
-      class={`flex items-center gap-2 py-1.5 rounded-md
+  <div>
+    <button class="w-full" on:click={() => (isOpen = !isOpen)}>
+      <div
+        class={`flex items-center gap-2 py-1 rounded-md
       ${active ? 'text-white' : 'text-gray-400'}`}
-    >
-      <slot />
+      >
+        <slot />
 
-      <i
-        class="icon-[tabler--chevron-down] ml-auto duration-200"
-        class:rotate-180={isOpen}
-      />
-    </div>
+        <i
+          class="icon-[tabler--chevron-down] ml-auto duration-200"
+          class:rotate-180={isOpen}
+        />
+      </div>
+    </button>
 
     {#if isOpen}
-      <div class="py-1 pl-8" transition:slide={{ duration: 200, axis: 'y' }}>
+      <div class="py-1 pl-6" transition:slide={{ duration: 200, axis: 'y' }}>
         <slot name="submenu" />
       </div>
     {/if}
-  </button>
+  </div>
 {/if}
